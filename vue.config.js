145e24230120e45aa2534993274f9af987a9db07
Alexey Ovchinnikov <perfektion.pro@ya.ru>
2020-11-03 12:34:40 +0300

    Отредактировал настройки для github pages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  publicPath: '/iamkrepysh/',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/iamkrepysh/'
+    : '/',
   css: {
     loaderOptions: {
       sass: {
